a40be46a5fa05e60748dfbff57660bfad1a99ded
Annie <annie.the.web.coder@gmail.com>
2024-02-29 15:50:10 -0500

    revise todolist and add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
+import AddTodoForm from './AddTodoForm'
 import './App.css'
+import TodoList from './TodoList'
 
 function App() {
-
-  const todoList = [
-    {
-      id: 1, 
-      title: "Complete Assigment"
-    },
-    {
-      id: 2,
-      title: "Submit Assignment"
-    },
-    {
-      id: 3,
-      title: "Wait for the review"
-    }
-  ]
 
   return (
     <>
       <h1>Todo List</h1>
-      <ul>
-        {
-          todoList.map(function (item) {
-            return <li key={item.id}>{item.title}</li>
-          })
-        }
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </>
   )
 }
