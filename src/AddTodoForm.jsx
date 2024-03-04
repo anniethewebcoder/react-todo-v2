91c5a3156504f1b3cba3664d6f33de71571be7ca
Annie <annie.the.web.coder@gmail.com>
2024-03-04 11:56:44 -0500

    input useState

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,9 +1,20 @@
-function AddTodoForm() {
+function AddTodoForm(props) {
+
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        
+        const todoTitle = event.target.title.value
+        console.log(todoTitle)
+        props.onAddTodo(todoTitle)
+
+        
+    }
+
     return (
         <>
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title</label>
-            <input id="todoTitle" type="text" />
+            <input id="todoTitle" type="text" name="title" />
             <button type="submit">Add</button>
         </form>
         </>
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import AddTodoForm from './AddTodoForm'
 import './App.css'
 import TodoList from './TodoList'
+import { useState } from 'react'
 
 function App() {
+
+  const [newTodo, setNewTodo] = useState()
 
   return (
     <>
       <h1>Todo List</h1>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>{newTodo}</p>
       <TodoList />
     </>
   )
